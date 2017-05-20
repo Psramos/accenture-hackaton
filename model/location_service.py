dfe44c9775e879750af5f11566a8159c432260dc
psramos <pau.sramos@gmail.com>
2017-05-20 12:15:09 +0200

    Added fixtures

--- model/location_service.py
+++ model/location_service.py
@@ -1,9 +1,28 @@
+import json
+import os
+
+
 class LocationService:
     def __init__(self):
         pass
 
     def get_all_locations(self, lat, lon, distance):
-        return "All locations with lat %s lon %s distance %s" % (lat, lon, distance)
+        return json.dumps(
+           [
+                {
+                    "id": "paraguay",
+                    "lat":"41.4172754",
+                    "lon":"2.206039",
+                    "score": 50
+                },
+                {
+                    "id": "bburguer",
+                    "lat":"41.3807892",
+                    "lon":"2.1478704",
+                    "score": 100
+                }
+            ]
+        )
 
     def get_location(self, id):
         return "Location with id %s" % id
